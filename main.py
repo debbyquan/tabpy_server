e556cf6433a56b670028501d5f13b74ce341aba7
debbyquan <125091326+debbyquan@users.noreply.github.com>
2026-03-03 16:34:26 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import os
-import subprocess
-import sys
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", "9004")
-
-    # Run TabPy's installed console command
-    cmd = ["tabpy-server", "--host", "0.0.0.0", "--port", str(port)]
-
-    # Replace this process with tabpy-server
-    os.execvp(cmd[0], cmd)
+    os.execvp(
+        "tabpy-server",
+        ["tabpy-server", "--host", "0.0.0.0", "--port", str(port)],
+    )
